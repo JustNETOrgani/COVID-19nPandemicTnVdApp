91988dc959843dc9d257a4f778952c625dedc44d
JustNET <justnetonline@outlook.com>
2020-12-30 02:19:38 +0800

    More implementation plus signature done

--- src/views/Verifier/verify.vue
+++ src/views/Verifier/verify.vue
@@ -23,10 +23,10 @@
                                 <el-input v-model="verificationForm.ifpsHash" placeholder="Enter IPFS hash."></el-input>
                             </el-form-item>
                             <el-row>
-                                <el-col :span="9" :offset="0">
+                                <el-col :span="5" :offset="0">
                                     <p id="computedLabel">Person signature:</p>
                                 </el-col>
-                                <el-col :span="6" :offset="0">
+                                <el-col :span="5" :offset="0">
                                     <p id="formattedString">{{sigOnIPFShash}}</p>
                                 </el-col>
                             </el-row>
@@ -43,16 +43,22 @@
 </template>
 
 <script>
+import ethEnabled from '@/assets/js/web3nMetaMask'
+import * as signingByAHP from '@/assets/js/sigHelperFns'
+import getHash from '@/assets/js/hashFunc'
+const ipfs = new window.Ipfs()
 
 export default {
   // name: 'Home',
   data () {
     return {
       verificationForm: {
-        ifpsHash: '',
-        verifyBtnLoadState: false
+        ifpsHash: ''
       },
+      hEcDR: '',
       sigOnIPFShash: '',
+      fullSignature: '',
+      personAccount: '',
       verifyBtnLoadState: false,
       rules: {
         ifpsHash: [
@@ -63,14 +69,72 @@ export default {
     }
   },
   created () {
-
+    if (!ethEnabled()) {
+      this.$message('Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!')
+    } else {
+      this.loadingPOnboardingPage = false
+      this.getAccount().then(accounts => {
+        this.personAccount = accounts[0]
+        console.log('Current account: ', this.personAccount)
+      })
+    }
   },
   methods: {
+    async getAccount () {
+      var accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      return accounts
+    },
     backToPrvPg () {
       this.$router.push('/')
     },
     resetForm (formName) {
       this.$refs[formName].resetFields()
+    },
+    submitForm (formName) {
+      if (this.verificationForm.ifpsHash !== '') {
+        if (this.ipfsInputValidation(this.verificationForm.ifpsHash) !== 0) {
+          this.$refs[formName].validate(valid => {
+            this.verifyBtnLoadState = true
+            if (valid) {
+              var data = {
+                ipfsHash: this.verificationForm.ifpsHash
+              }
+              // Steps ---> TODO
+              // Acquire encrypted data on IPFS.
+              ipfs.cat(data.ipfsHash).then(retrievedData => {
+                var EcDRwithSig = JSON.parse(retrievedData.toString()) // Convert to string and parse as JSON object.
+                console.log('Encrypted data: ', EcDRwithSig)
+                // Hash the EcDR.
+                getHash(EcDRwithSig.encryptedData).then(res => {
+                  this.hEcDR = res
+                  // Allow user to sign IPFS hash.
+                  signingByAHP.signatureGen(data.ipfsHash, this.personAccount, (sig) => {
+                    this.fullSignature = sig
+                    this.sigOnIPFShash = (sig.substring(0, 25) + '...' + sig.substr(sig.length - 25)).replace(/"/g, '') // Remove the double quotes.
+                    console.log('Person sig.: ', this.fullSignature)
+                    // Verify on-chain
+                    this.verifyBtnLoadState = false
+                  })
+                })
+              })
+            } else {
+              console.log('Submission error.')
+              this.verifyBtnLoadState = false
+              return false
+            }
+          })
+        } else {
+          this.$message({
+            message: 'Sorry! Invalid IPFS hash entered. Please, re-enter.',
+            type: 'warning'
+          })
+        }
+      } else {
+        this.$message({
+          message: 'Sorry! IPFS field cannot be empty.',
+          type: 'warning'
+        })
+      }
     },
     ipfsInputValidation (input) {
       const count = input.toString().length
@@ -114,11 +178,12 @@ export default {
 }
 #computedLabel{
   text-align: left;
-  font-size: 1rem;
+  font-size: 0.8rem;
   color: rgb(113, 140, 189);
 }
 #formattedString{
-  font-size: 0.71rem;
+  font-size: 0.75rem;
+  text-align: left;
   font-style: italic;
   color: rgb(95, 64, 116);
 }
