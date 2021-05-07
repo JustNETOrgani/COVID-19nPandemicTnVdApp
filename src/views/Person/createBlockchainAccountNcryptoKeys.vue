b760a8ad946d6ac5b5505e92e7315f64851194c7
JustNETlab <justnetonline@outlook.com>
2021-05-07 18:59:17 +0800

    Added comments for Person address update

--- src/views/Person/createBlockchainAccountNcryptoKeys.vue
+++ src/views/Person/createBlockchainAccountNcryptoKeys.vue
@@ -24,6 +24,14 @@
                 </el-col>
             </el-row>
         </div>
+        <div class="wrapper">
+            <h2>Update address on the blockchain</h2>
+            <el-row>
+                <el-col :span="12" :offset="6">
+                  <el-button type="primary" :loading="addrUpdateBtnLoadState" @click="addrUpdate()">Create update</el-button>
+                </el-col>
+            </el-row>
+        </div>
         <div id="overlay">
           <div id="cryptoContainer">
             <el-row>
@@ -49,22 +57,93 @@
           </div>
           <el-button type="primary" @click="closeDiv()">Close</el-button>
         </div>
+        <el-dialog title="Addres update" :visible.sync="addrUpdateDialogFormVisible" width="30%">
+          <el-form :model="addressUpdateForm" :rules="rules" ref="addressUpdateForm">
+            <el-form-item label="Hashed ID" prop="hid">
+              <el-input v-model="addressUpdateForm.hid" placeholder="Enter Hashed ID"></el-input>
+            </el-form-item>
+            <el-form-item label="IPFS hash" prop="ifpsHash">
+              <el-input v-model="addressUpdateForm.ifpsHash" placeholder="Enter IPFS hash"></el-input>
+            </el-form-item>
+          </el-form>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="addrUpdateDialogFormVisible = false">Cancel</el-button>
+            <el-button type="primary" @click="performAddrUpdate('addressUpdateForm')">Confirm</el-button>
+          </span>
+        </el-dialog>
     </div>
 </template>
 
 <script>
+import ethEnabled from '@/assets/js/web3nMetaMask'
+import * as signatureGenerator from '@/assets/js/sigHelperFns'
+import getHash from '@/assets/js/hashFunc'
+import { ABI, contractAddress, suppliedGas } from '@/assets/js/contractABI'
+import web3 from '@/assets/js/web3Only'
 
 export default {
   data () {
     return {
       keyGenBtnLoadState: false,
       prvKey: null,
-      pubKey: null
+      pubKey: null,
+      addressUpdateForm: {
+        hid: '',
+        ifpsHash: ''
+      },
+      personAddress: '',
+      newPersonAddress: '',
+      accountChangeStatus: false,
+      addrUpdateDialogFormVisible: false,
+      addrUpdateBtnLoadState: false,
+      rules: {
+        hid: [
+          { required: true, message: 'Please input HID', trigger: 'blur' },
+          { min: 46, message: 'Length should be at least 46', trigger: 'blur' }
+        ],
+        ifpsHash: [
+          { required: true, message: 'Please input IPFS hash', trigger: 'blur' },
+          { min: 46, message: 'Length should be at least 46', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  created () {
+    if (!ethEnabled()) {
+      this.$message('Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!')
+    } else {
+      this.getAccount().then(accounts => {
+        this.personAddress = accounts[0]
+        console.log('Current account: ', this.personAddress)
+      })
+    }
+  },
+  watch: {
+    'personAddress' () {
+      this.switchAccount()
     }
   },
   methods: {
     backToPrvPg () {
       this.$router.push('/')
+    },
+    async getAccount () {
+      var accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      return accounts
+    },
+    switchAccount () {
+      var myRoot = this // Ensure all this or vue global variables can be accessed within this fucntion via myRoot.
+      window.ethereum.on('accountsChanged', function (accounts) {
+        myRoot.newPersonAddress = accounts[0]
+        myRoot.personAddress = accounts[0]
+        console.log('Selected account: ', myRoot.newPersonAddress)
+        myRoot.$message({
+          message: 'Account switched successfully.',
+          type: 'success'
+        })
+        console.log('Account switched')
+        myRoot.accountChangeStatus = true
+      })
     },
     keyGen () {
       this.keyGenBtnLoadState = true
@@ -134,6 +213,104 @@ export default {
     },
     closeDiv () {
       document.getElementById('overlay').style.display = 'none'
+    },
+    async sendTnx (txParams) {
+      // Transaction execution in Ethereum from Metamask
+      var txReceipt = await window.ethereum.request({ method: 'eth_sendTransaction', params: [txParams] })
+      return txReceipt
+    },
+    addrUpdate () {
+      this.addrUpdateBtnLoadState = true
+      this.addrUpdateDialogFormVisible = true
+    },
+    performAddrUpdate (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log('Address update initiated')
+          this.addrUpdateDialogFormVisible = false
+          var dataEntered = {
+            personHID: this.addressUpdateForm.hid,
+            personIPFShash: this.addressUpdateForm.ifpsHash
+          }
+          getHash(dataEntered.personIPFShash).then(hIPFShash => {
+            console.log('Hash computed.')
+            // Get Signature from person.
+            signatureGenerator.signatureGen(hIPFShash, this.personAddress, (sig) => {
+              console.log('Signature generated')
+              this.$message('Please switch account to new account and sign again')
+              this.$confirm('Account switched?.', 'Warm tip', {
+                confirmButtonText: 'Yes',
+                cancelButtonText: 'No',
+                type: 'warning'
+              }).then(() => {
+                if (this.accountChangeStatus === true) {
+                  this.pushTransactionToBlockchain(dataEntered.personHID, hIPFShash, sig)
+                } else {
+                  this.$message.error('Oops, account not changed.')
+                  this.addrUpdateBtnLoadState = false
+                }
+              }).catch(() => {
+                this.$message({
+                  type: 'info',
+                  message: 'User canceled'
+                })
+                this.addrUpdateBtnLoadState = false
+              })
+            }).catch(err => {
+              console.log('Sorry! Error occured.', err)
+              this.addrUpdateBtnLoadState = false
+              this.$message.error('Error getting signature. Please try again later.')
+            })
+          })
+        } else {
+          console.log('Submission error.')
+          this.$message({
+            message: 'Please, fill the form.',
+            type: 'warning'
+          })
+          this.addrUpdateBtnLoadState = false
+          return false
+        }
+      })
+    },
+    pushTransactionToBlockchain (personHID, hIPFShash, sig) {
+      console.log('Sending transaction on chain')
+      var blockCovid = new web3.eth.Contract(ABI, contractAddress, { defaultGas: suppliedGas })
+      console.log('Contract instance created.')
+      // Smart contract and other logic continues.
+      try {
+        // Transaction parameters
+        const txParams = {
+          from: this.personAddress,
+          to: contractAddress,
+          data: blockCovid.methods.updatePersonBlockchainAddr(this.newPersonAddress, personHID, hIPFShash, sig).encodeABI()
+        }
+        this.sendTnx(txParams).then(tnxReceipt => {
+          console.log('Transaction receipt: ', tnxReceipt)
+          this.$message({
+            type: 'info',
+            message: 'Transaction successful'
+          })
+          this.addrUpdateBtnLoadState = false
+          this.$alert('Address successfully updated on BlockCovid-19.', 'Address update success', {
+            confirmButtonText: 'OK',
+            callback: action => {
+              this.$message({
+                type: 'info',
+                message: 'Transaction successful'
+              })
+            }
+          })
+        }).catch(err => {
+          console.log('Sorry! Error occured.', err)
+          this.addrUpdateBtnLoadState = false
+          this.$message.error('Blockchain error. Please try again later.')
+        })
+      } catch {
+        console.log('Sorry! Error occured.')
+        this.addrUpdateBtnLoadState = false
+        this.$message.error('Non-transactional error. Please try again later.')
+      }
     }
   },
   computed: {
